7c4b4cc7b092b3f97a27ce4c9ddaf2736c9b0aad
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-11-08 19:10:34 -0800

    binario

--- binario.c
+++ binario.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
 
 /**
  * rev_string - Function that change the order of a string.
  * @s: string to be write inverse.
- *
+ *Return: reverse string
  */
 
 char *rev_string(char *s)
@@ -25,57 +27,62 @@ char *rev_string(char *s)
 		s[d] = temporal;
 	}
 
-    return(s);
+return (s);
 }
 
-int main(void)
-{
+/**
+*imprime_binary - generate a binary
+*@args: num to convert
+*Return: number of characters.
+*/
 
-int j = 0, i = 0, num = 0, n, no = 0;
+int imprime_binary(va_list args)
+{
+unsigned int j = 0, n;
+int cuenta = 0;
 char *s, *sfin;
 
-n = no;
+n = va_arg(args, unsigned int);
 while (n > 1)
 {
-    n /= 2;
-    j++;
+	n /= 2;
+	j++;
 }
 j = j + 1;
 sfin = malloc(j + 1);
+if (sfin == NULL)
+	return (-1);
+
 s = sfin = malloc(j + 1);
-
-n = no;
-if (no == 0)
+if (n == 0)
 {
-    s[0] = '0';
-    s[1] = '\0';
+	s[0] = '0';
+	s[1] = '\0';
+	cuenta = 1;
 }
-
 while (n > 0)
 {
-    if (n % 2 == 0)
-    {
-        s[i] = 0 + '0';
-    }
+	if (n % 2 == 0)
+	{
+		s[cuenta] = 0 + '0';
+	}
 
-    if (n % 2 == 1)
-    {
-        s[i] = 1 + '0';
-    }
-    n /= 2;
-    if (n == 0)
-    {
-        i++;
-        s[i] = 1 + '0';
-        break;
-    }
-    i++;
+	if (n % 2 == 1)
+	{
+		s[cuenta] = 1 + '0';
+	}
+	n /= 2;
+	if (n == 0)
+	{
+		cuenta++;
+		s[cuenta] = 1 + '0';
+		break;
+	}
+cuenta++;
 }
-if(no !=0)
-    s[i] = '\0';
-
+if (n != 0)
+    s[cuenta] = '\0';
 sfin = rev_string(s);
-printf("%s\n", sfin);
 free(sfin);
-return (i);
+return (cuenta);
 }
